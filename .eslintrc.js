c8e84f0e78fd016bba4d5bcf744d8e1f8a27bf82
Swagato Chakraborty <Swagato1024@users.noreply.github.com>
2023-08-26 12:25:09 +0530

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+
+  rules: {
+    "semi": ["warn", "always"],
+    "quotes": ["warn", "double"],
+    "no-multiple-empty-lines": "error",
+    "key-spacing": ["error", { afterColon: true }],
+
+    "no-global-assign": "error",
+    "no-cond-assign": ["warn", "always"],
+    "id-length": ["warn", { max: 30 }],
+    "max-statements": ["warn", 15],
+
+    "multiline-ternary": ["warn", "never"],
+    "no-unneeded-ternary": ["error", { defaultAssignment: false }],
+    "no-nested-ternary": "error",
+
+    "space-infix-ops": "warn",
+    "space-before-blocks": ["warn", "always"],
+    "no-mixed-operators": "error",
+
+    "camelcase": ["error", { properties: "never" }],
+    "new-cap": ["error", { newIsCap: true }],
+    "prefer-const": "error",
+  },
+};
