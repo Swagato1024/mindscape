baa3e4614ae2142f4c27f71031665a3ae848f62d
Swagato Chakraborty <Swagato1024@users.noreply.github.com>
2023-08-29 13:16:17 +0530

    create users.json and articles.json if not exists

--- src/loader.js
+++ src/loader.js
@@ -2,12 +2,24 @@ const fs = require("fs");
 const Articles = require("./models/articles");
 
 const loadUsers = () => {
-  const users = fs.readFileSync("./storage/users.json");
+  const filepath = "./storage/users.json";
+
+  if (!fs.existsSync(filepath)) {
+    fs.writeFileSync(filepath, "[]");
+  }
+
+  const users = fs.readFileSync(filepath);
   return JSON.parse(users);
 };
 
 const loadArticles = () => {
-  const rawArticles = fs.readFileSync("./storage/articles.json");
+  const filepath = "./storage/articles.json";
+
+  if (!fs.existsSync(filepath)) {
+    fs.writeFileSync(filepath, "[]");
+  }
+  
+  const rawArticles = fs.readFileSync(filepath);
   return new Articles(JSON.parse(rawArticles));
 };
 
